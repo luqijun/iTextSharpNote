4c70d339240f28b37d10f67977e05993f2a20342
lqjun <lqjun@epoint.com.cn>
2021-06-23 11:16:45 +0800

    添加基于文件流合并PDF方法。

--- EmbeddedFont_Net4/App.xaml.cs
+++ EmbeddedFont_Net4/App.xaml.cs
@@ -21,31 +21,45 @@ namespace EmbeddedFont_Net4
             base.OnStartup(e);
 
 
-            FileStream fs = new FileStream(@"C:\Users\Administrator\Desktop\123.pdf", FileMode.Create);
-            Document doc = new Document(PageSize.A4, 36f, 36f, 36f, 36f);
 
-            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
-            doc.Open();
+            {
+                //FileStream fs = new FileStream(@"C:\Users\Administrator\Desktop\123.pdf", FileMode.Create);
+                //Document doc = new Document(PageSize.A4, 36f, 36f, 36f, 36f);
 
-            string fontPath = @"C:\Windows\Fonts\";
-            BaseFont bfSong = BaseFont.CreateFont(fontPath + "simsunb.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-            Font font = new Font(bfSong, 16, 1, BaseColor.BLACK);
-            PdfContentByte cb = writer.DirectContent;
-            cb.SetFontAndSize(bfSong, 16);
+                //PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+                //doc.Open();
+                //doc.NewPage();
 
-            doc.NewPage();
 
-            doc.Add(new iTextSharp.text.Paragraph("Hello World! Hello People! " +
-            "Hello Sky! Hello Sun! Hello Moon! Hello Stars!"));
+                //string fontPath = @"C:\Users\Administrator\Desktop\";
+                //BaseFont bfSong = BaseFont.CreateFont(fontPath + "EpointSpecialFont2.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                //Font font = new Font(bfSong, 16, 1, BaseColor.BLACK);
+                //PdfContentByte cb = writer.DirectContent;
+                //cb.BeginText();
+                //cb.SetFontAndSize(bfSong, 16);
 
-            //doc.AddTitle("这里是标题");
-            //doc.AddSubject("主题");
-            //doc.AddKeywords("关键字");
-            //doc.AddCreator("创建者");
-            //doc.AddAuthor("作者");
+                //cb.ShowTextAligned(PdfContentByte.ALIGN_LEFT, "测试文本", 100, 100, 0);
 
-            doc.Close();
-            fs.Close();
+                //doc.Add(new iTextSharp.text.Paragraph("Hello World! Hello People! Hello Sky! Hello Sun! Hello Moon! Hello Stars!", font));
+
+                //doc.AddTitle("这里是标题");
+                //doc.AddSubject("主题");
+                //doc.AddKeywords("关键字");
+                //doc.AddCreator("创建者");
+                //doc.AddAuthor("作者");
+
+
+                //cb.EndText();
+
+                //doc.Close();
+                //fs.Close();
+            }
+
+            MergeFilesHelper.MergeFiles(@"C:\Users\Administrator\Desktop\Merge");
+
+
+            MessageBox.Show("生成成功！");
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ EmbeddedFont_Net4/MergeFilesHelper.cs
@@ -0,0 +1,189 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EmbeddedFont_Net4
+{
+    /// <summary>
+    /// 基于文件流进行读取
+    /// </summary>
+    public static class MergeFilesHelper
+    {
+        public static string MergeFiles(string targetPdfFilesDir)
+        {
+            string outPath = string.Empty;
+            //验证文件是否存在
+            if (!Directory.Exists(targetPdfFilesDir))
+            {
+                throw new FileNotFoundException("指定的目录不存在：" + targetPdfFilesDir);
+            }
+
+            var filePathList = Directory.EnumerateFiles(targetPdfFilesDir, "*.pdf");
+            if (!filePathList.Any())
+            {
+                return outPath;
+            }
+
+            //合并pdf文件
+
+            string runningDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            //outPath = Path.Combine(runningDir, "temp", Guid.NewGuid().ToString() + ".pdf");
+            outPath = @"C:\Users\Administrator\Desktop\aa.pdf";
+
+            MergeFiles(outPath, filePathList.ToArray());
+
+            return outPath;
+        }
+
+
+        public static void MergeFiles(string outPath, string[] pdfFiles)
+        {
+
+            try
+            {
+
+                if (pdfFiles == null || pdfFiles.Length <= 0)
+                {
+                    return;
+                }
+                if (pdfFiles.Length == 1)
+                {
+                    return;
+                }
+
+                PdfReader reader;
+                Document document;
+                PdfWriter writer;
+
+                using (FileStream fs = new FileStream(outPath, FileMode.Create))
+                {
+                    reader = new PdfReader(pdfFiles[0]);
+                    using (document = new Document())
+                    {
+                        //一个PdfSmartCopy基类
+                        writer = new PdfSmartCopy(document, fs);
+                        document.Open();
+
+                        for (int k = 0; k < pdfFiles.Length; k++)
+                        {
+                            reader = new PdfReader(pdfFiles[k]);
+                            //将子文件中的页都追加到尾部
+                            for (int i = 1; i < reader.NumberOfPages + 1; i++)
+                            {
+                                ((PdfSmartCopy)writer).AddPage(writer.GetImportedPage(reader, i));
+                            }
+                            writer.FreeReader(reader);
+                        }
+                        reader.Close();
+                        writer.Close();
+                        document.Close();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                string strOb = e.Message;
+            }
+        }
+    }
+
+    public static class MergeFilesHelper_Old
+    {
+        public static void MergeFiles(string destinationFile, string[] sourceFiles)
+        {
+
+            try
+            {
+                int f = 0;
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(sourceFiles[f]);
+                // we retrieve the total number of pages
+                int n = reader.NumberOfPages;
+                //Console.WriteLine("There are " + n + " pages in the original file.");
+                // step 1: creation of a document-object
+                Document document = new Document(reader.GetPageSizeWithRotation(1));
+                // step 2: we create a writer that listens to the document
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(destinationFile, FileMode.Create));
+                // step 3: we open the document
+                document.Open();
+                PdfContentByte cb = writer.DirectContent;
+                PdfImportedPage page;
+                int rotation;
+                // step 4: we add content
+                while (f < sourceFiles.Length)
+                {
+                    int i = 0;
+                    while (i < n)
+                    {
+                        i++;
+                        document.SetPageSize(reader.GetPageSizeWithRotation(i));
+                        document.NewPage();
+                        page = writer.GetImportedPage(reader, i);
+                        rotation = reader.GetPageRotation(i);
+                        if (rotation == 90 || rotation == 270)
+                        {
+                            cb.AddTemplate(page, 0, -1f, 1f, 0, 0, reader.GetPageSizeWithRotation(i).Height);
+                        }
+                        else
+                        {
+                            cb.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+                        }
+                        //Console.WriteLine("Processed page " + i);
+                    }
+                    f++;
+                    if (f < sourceFiles.Length)
+                    {
+                        reader = new PdfReader(sourceFiles[f]);
+                        // we retrieve the total number of pages
+                        n = reader.NumberOfPages;
+                        //Console.WriteLine("There are " + n + " pages in the original file.");
+                    }
+                }
+                // step 5: we close the document
+                document.Close();
+            }
+            catch (Exception e)
+            {
+                string strOb = e.Message;
+            }
+        }
+
+        public static void Merge(List<String> InFiles, String OutFile)
+        {
+
+            using (FileStream stream = new FileStream(OutFile, FileMode.Create))
+            using (Document doc = new Document())
+            using (PdfCopy pdf = new PdfCopy(doc, stream))
+            {
+                doc.Open();
+
+                PdfReader reader = null;
+                PdfImportedPage page = null;
+
+                //fixed typo
+                InFiles.ForEach(file =>
+                {
+                    reader = new PdfReader(file);
+
+                    for (int i = 0; i < reader.NumberOfPages; i++)
+                    {
+                        page = pdf.GetImportedPage(reader, i + 1);
+                        pdf.AddPage(page);
+                    }
+
+                    pdf.FreeReader(reader);
+                    reader.Close();
+                    File.Delete(file);
+                });
+            }
+        }
+    }
+}
+
+
